e2fe7634ed764ff11852ce2f02a9333b12066020
healthcarecanteen-3228 <healthcarecanteen@gmail.com>
2025-07-21 05:16:02 +0000

    okey

--- dist/models/role.model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const RoleSchema = new mongoose_1.Schema({
-    name: { type: String, required: true, unique: true },
-    permissions: [{ type: String }],
-});
-exports.default = (0, mongoose_1.model)('Role', RoleSchema);
